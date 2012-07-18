02a5047e8fa13b1beb223a856daf769dd6bd9b17
Brendan <ex.actionmodel@gmail.com>
2012-07-18 08:56:39 -0500

    Init repo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Missioncreek::Application.config.secret_token = '814874ed7baf265b48876d315108189504b0b36c19b59281bf4c7d4183d0bc3c56cd4b8e4c920513f29a05959564b1db537cb284a41a94085bf625981e92e92e'
