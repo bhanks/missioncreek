f237ab4343ac43be4dc138b95d69173b96cd3781
bhanks <ex.actionmodel@gmail.com>
2012-07-31 13:00:30 -0500

    Initial dashboard work completed.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,18 +2,28 @@ class DashboardController < ApplicationController
 	layout "dashboard.html.erb"
 	before_filter :authenticate_user!
 
+
+	def index
+		@artists = Artist.all
+		@venues = Venue.all
+		@posts = Post.all
+	end
+
 	def artists
 		@artists = Artist.all
+		@artist_active = "active"
 		#render 'artists/admin_index'
 	end
 
 	def venues
 		@venues = Venue.all
+		@venue_active = "active"
 		#render 'venues/index'
 	end
 
 	def posts
 		@posts = Post.all
+		@post_active = "active"
 	end
 
 end
