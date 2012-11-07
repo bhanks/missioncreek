2e8a57bfd5695cecc69d65d6055667205395d6af
bhanks <ex.actionmodel@gmail.com>
2012-11-07 10:53:44 -0600

    Turned off assets precompile for the moment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Missioncreek::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
   config.encoding = 'utf-8'
   # Generate digests for assets URLs
   config.assets.digest = true
