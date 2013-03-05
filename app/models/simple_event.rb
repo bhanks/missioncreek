22ff874f6415a2958c0feae0bd5289614de5d7fd
bhanks <ex.actionmodel@gmail.com>
2013-03-05 16:48:13 -0600

    added lightbox

--- app/models/simple_event.rb
+++ app/models/simple_event.rb
@@ -2,4 +2,14 @@ class SimpleEvent < ActiveRecord::Base
   attr_accessible :date, :description, :free, :image, :price, :soldout, :start, :ticket_url, :title, :category, :venue_id
   mount_uploader :image, SimpleEventImageUploader
   belongs_to :venue
+
+  def display_price
+	price = ""
+	unless self.free || self.soldout
+		price = self.price
+	else
+		price = (self.free) ? "This is event is FREE" : "This event is SOLDOUT"
+	end
+	price
+  end
 end
