ee4303421eb47e6cde2fa3412c0f2d7c2e599b10
Brendan Hanks <ex.actionmodel@gmail.com>
2013-12-09 12:48:18 -0600

    Splash milestone.

---
+++ public/2014.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  
+  $('#more_info').on('click',function(){
+    $('.primary, .general, .info_nav, #credit_union, #credit_info').toggle();
+    $('#credit_info').css('display', 'inline-block');
+    $('.navigation').attr('src', '2014_img/info.png');
+  });
+  $('.back_nav').on('click', function(){
+    $('.primary, .general, .info_nav, #credit_union, #credit_info').toggle();
+    $('.navigation').attr('src', '2014_img/first.png');
+
+  });
+  $('.general_nav').on('click', function(){
+    console.log('clicked general')
+    if($('.passes').is(':visible'))
+    {
+      $('.passes, .general').toggle();  
+    }
+  });
+  $('.pass_nav').on('click', function(){
+    console.log('clicked pass info')
+    if($('.general').is(':visible'))
+    {
+      $('.passes, .general').toggle();  
+    }
+  });
+
+});
