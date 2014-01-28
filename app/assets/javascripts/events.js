dee51e8f53ccc6eab805b11699bbfd6ecf027a71
Brendan Hanks <ex.actionmodel@gmail.com>
2014-01-28 09:49:53 -0600

    finished toggling details

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,1 +1,24 @@
+$(document).ready(function(){
 
+  if($('#calender_page')!=[])
+  {
+    $('span.glyphicon-plus-sign').each(function(){
+      var event = $(this).closest(".event");
+      $(this).on("click", function(){
+        if($(this).hasClass("glyphicon-plus-sign"))
+        {
+          $(this).removeClass("glyphicon-plus-sign");
+          $(this).addClass("glyphicon-minus-sign");
+          $(".details."+$(event).attr("id")).slideDown(200);
+        }
+        else
+        {
+          $(this).addClass("glyphicon-plus-sign");
+          $(this).removeClass("glyphicon-minus-sign");
+          $(".details."+$(event).attr("id")).slideUp(200);
+        }
+      });
+    }); 
+  }
+
+});
