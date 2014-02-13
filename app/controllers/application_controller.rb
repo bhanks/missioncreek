cb097cf5e824e0aa31590155af7d9ba2e7c4a425
Brendan Hanks <ex.actionmodel@gmail.com>
2014-02-13 15:18:15 -0600

    hotfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :nav_paths
-  def nav_paths
-	@nav_paths = {}
-	@nav_paths[:about] = (Page.exists?(:title=>"About")) ? Rails.application.routes.url_helpers.page_path(Page.where("title = ?", "About").first) : "javascript:void(0)"
-	@nav_paths[:lit] = (Page.exists?(:title=>"Literature")) ? Rails.application.routes.url_helpers.page_path(Page.where("title = ?", "Literature").first) : "javascript:void(0)"
-	@nav_paths[:food] = (Page.exists?(:title=>"Food")) ? Rails.application.routes.url_helpers.page_path(Page.where("title = ?", "Food").first) : "javascript:void(0)"
-	@nav_paths[:residency] = (Page.exists?(:title=>"Artist Residency")) ? Rails.application.routes.url_helpers.page_path(Page.where("title = ?", "Artist Residency").first) : "javascript:void(0)"
-	@nav_paths[:schedule] = (Page.exists?(:title=>"Schedule")) ? Rails.application.routes.url_helpers.page_path(Page.where("title = ?", "Schedule").first) : "javascript:void(0)"
-  end
 
 end
--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,9 +1,6 @@
 class BaseController < ApplicationController
 
 	def index
-		@posts = Post.find(:all, :order=>"created_at DESC")
-		@artists = Artist.where("front_page != ?", 0).order("front_page")
-		@features = Page.where("feature_order IS NOT NULL").order("feature_order")
 		
 		render :layout => "application"
 	end
