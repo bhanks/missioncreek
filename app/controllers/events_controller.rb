3ff7377e58916838fe8917a0fff5cda0b705fa66
Brendan Hanks <ex.actionmodel@gmail.com>
2014-02-28 09:19:05 -0600

    json for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,7 +21,7 @@ class EventsController < ApplicationController
     respond_to do |format|
       format.html {render :layout =>"application"}
       format.json {
-        @events = Event.where(:visible=>true).tagged_with("tech",:exclude=>true).order(:door_time)
+        @events = Event.where(:visible=>true).tagged_with("tech",:exclude=>true).order(:date, :door_time)
       } 
     end
   end
